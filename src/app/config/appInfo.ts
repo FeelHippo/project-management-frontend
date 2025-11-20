99435fbb0f9bb2c6e923d5f832c55d1c84c3cd49
FeelHippo <mezzasegolas@gmail.com>
2025-11-20 12:23:23 +0100

    chore: prepare for migration

--- src/app/config/appInfo.ts
+++ src/app/config/appInfo.ts
@@ -1,6 +1,6 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
-  appName: 'nextjs-prisma-supertokens',
+  appName: 'rityra-web-app',
   websiteDomain: process.env.WEBSITE_DOMAIN ?? 'http://localhost:3000',
   apiDomain: process.env.API_DOMAIN ?? 'http://localhost:8080',
   apiBasePath: '/auth',
