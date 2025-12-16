2dab7785146faed49bb9d95da8a895aea68a84a2
FeelHippo <mezzasegolas@gmail.com>
2025-12-16 20:58:04 +0100

    chore(AWS): deployed and aligned env vars

--- src/app/config/appInfo.ts
+++ src/app/config/appInfo.ts
@@ -1,7 +1,7 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: 'project-management',
-  websiteDomain: process.env.AWS_AMP_DOMAIN ?? 'http://localhost:3000',
-  apiDomain: process.env.AWS_EBS_DOMAIN ?? 'http://localhost:8080',
+  websiteDomain: process.env.AMP_DOMAIN ?? 'http://localhost:3000',
+  apiDomain: process.env.EBS_DOMAIN ?? 'http://localhost:8080',
   apiBasePath: '/auth',
 };
