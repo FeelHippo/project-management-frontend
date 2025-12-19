4dd338494ede0c0c0afba2e3092c4bc6ca864517
FeelHippo <mezzasegolas@gmail.com>
2025-12-19 11:40:09 +0100

    fix: removed route guard to fix minified react bug

--- src/app/(home)/dashboard/page.tsx
+++ src/app/(home)/dashboard/page.tsx
@@ -20,14 +20,14 @@ import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 
 export default function Dashboard() {
   // TODO(Filippo): avoid flash navigation to dashboard if user has no session
-  // protect this route frm direct navigation
-  useEffect(() => {
-    (async () => {
-      if (!(await Session.doesSessionExist())) {
-        window.location.href = '/login';
-      }
-    })();
-  }, []);
+  // protect this route frm direct navigation. Warning: might be causing https://github.com/vercel/next.js/issues/78396
+  // useEffect(() => {
+  //   (async () => {
+  //     if (!(await Session.doesSessionExist())) {
+  //       window.location.href = '/login';
+  //     }
+  //   })();
+  // }, []);
 
   const queryClient = useQueryClient();
   const projectUid = queryClient.getQueryData(['detailUid']);
@@ -81,54 +81,50 @@ export default function Dashboard() {
 
   return (
     <div className="flex flex-row h-dvh w-full gap-12 px-12 py-3">
-      <ViewTransition>
         <Card className="h-fit w-2/3">
-          <CardHeader>
-            <CardTitle>Project Timeline</CardTitle>
-            <CardDescription>
-              Here you can see the most recent events:
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <TimelineLayout
-              items={timelineData}
-              size="lg"
-              iconColor="primary"
-              customIcon={<CalendarIcon />}
-            />
-          </CardContent>
+            <CardHeader>
+                <CardTitle>Project Timeline</CardTitle>
+                <CardDescription>
+                    Here you can see the most recent events:
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <TimelineLayout
+                    items={timelineData}
+                    size="lg"
+                    iconColor="primary"
+                    customIcon={<CalendarIcon />}
+                />
+            </CardContent>
         </Card>
-      </ViewTransition>
-      <ViewTransition>
         <Card className="h-60 w-1/3">
-          <CardHeader>
-            <CardTitle>{name}</CardTitle>
-            <CardDescription>{description}</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <ToggleGroup
-              type="multiple"
-              variant="outline"
-              spacing={2}
-              size="sm"
-              className="flex-wrap"
-            >
-              {tags.map((tag: string, index) => (
-                <Field key={index} className="w-fit">
-                  <ToggleGroupItem
-                    value={tag}
-                    aria-label="Toggle star"
-                    className="data-[state=on]:bg-transparent data-[state=on]:*:[svg]:fill-yellow-500 data-[state=on]:*:[svg]:stroke-yellow-500"
-                  >
-                    <Tag />
-                    {tag}
-                  </ToggleGroupItem>
-                </Field>
-              ))}
-            </ToggleGroup>
-          </CardContent>
+            <CardHeader>
+                <CardTitle>{name}</CardTitle>
+                <CardDescription>{description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ToggleGroup
+                    type="multiple"
+                    variant="outline"
+                    spacing={2}
+                    size="sm"
+                    className="flex-wrap"
+                >
+                    {tags.map((tag: string, index) => (
+                        <Field key={index} className="w-fit">
+                            <ToggleGroupItem
+                                value={tag}
+                                aria-label="Toggle star"
+                                className="data-[state=on]:bg-transparent data-[state=on]:*:[svg]:fill-yellow-500 data-[state=on]:*:[svg]:stroke-yellow-500"
+                            >
+                                <Tag />
+                                {tag}
+                            </ToggleGroupItem>
+                        </Field>
+                    ))}
+                </ToggleGroup>
+            </CardContent>
         </Card>
-      </ViewTransition>
     </div>
   );
 }
